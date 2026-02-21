617c64ceb12c17f955d811f640774600d031c5a3
hibiki <takechi.hibiki1234@gmail.com>
2026-02-21 17:25:51 +0900

    クイズの練習

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,49 @@
 // src/App.tsx
 
 import { useState } from "react";
-
-function TemperatureDisplay({temperature}:{temperature:number}){
-  return(
-    <>
-      <p>今の温度は{temperature}℃です</p>
-      {temperature >= 30 ? <p>熱いです</p>: <p>快適です</p>}
-    </>
-  );
+type IsCorrect = boolean|null
+function AnswerMessage({isCorrect}:{isCorrect:IsCorrect}){
+  if(isCorrect === null){
+    return <p>答えを選んでね</p>
+  }
+  return <p>{isCorrect ? "正解!" : "不正解..."}</p>
 }
-type SetTemperatureButtonProp = {
-  degree:number;
-  isRaise:boolean;
+type AnswerButtonProps = {
+  answer:string;
   clickAction:()=>void;
 }
-function SetTemperatureButton({degree, isRaise, clickAction}:SetTemperatureButtonProp){
-  return <button onClick={clickAction}>温度を{degree}℃{isRaise ? "上げる" : "下げる"}</button>
+function AnswerButton({answer, clickAction}:AnswerButtonProps){
+  return <button onClick={clickAction}>{answer}</button>
 }
-const initialTemperature = 20
-const degree = 5;
-export default function WeatherStation(){
-  const [currentTemperature, SetCurrentTemperature] = useState<number>(initialTemperature);
-  function clickSetTemperature(isRaise:boolean){
-    if(isRaise){
-      SetCurrentTemperature(currentTemperature + degree);
+type ResetButtonProps = {
+  clickAction:()=>void;
+}
+function ResetButton({clickAction}:ResetButtonProps){
+  return <button onClick={clickAction}>リセット</button>
+}
+export default function QuizGame(){
+  const [isCorrect, setIsCorrect] = useState<IsCorrect>(null);
+  const [selectedAnswer, setSelectedAnswer] = useState<string>("?");
+  function initializeQuiz(){
+    setIsCorrect(null);
+    setSelectedAnswer("?");
+  }
+  function checkAnswer(answer:string){
+    setSelectedAnswer(answer);
+    if(answer === "5"){
+      setIsCorrect(true);
     }
     else{
-      SetCurrentTemperature(currentTemperature - degree);
+      setIsCorrect(false);
     }
   }
   return(
     <>
-      <TemperatureDisplay temperature={currentTemperature}/>
-      <SetTemperatureButton clickAction={()=>clickSetTemperature(true)} degree={degree} isRaise={true}/>
-      <SetTemperatureButton clickAction={()=>clickSetTemperature(false)} degree={degree} isRaise={false}/>
+      <p>2 + 3 = {selectedAnswer}</p>
+      <AnswerMessage isCorrect={isCorrect}/>
+      <AnswerButton clickAction={()=>checkAnswer("4")} answer="4"/>
+      <AnswerButton clickAction={()=>checkAnswer("5")} answer="5"/>
+      {isCorrect !== null ? <ResetButton clickAction={()=>initializeQuiz()}/> : ""}
     </>
   );
 }
