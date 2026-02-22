42d0a261696221223899848af12cb2f3bee60ac9
hibiki <takechi.hibiki1234@gmail.com>
2026-02-22 13:00:28 +0900

    cart-practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,57 @@
 // src/App.tsx
 
 import { useState } from "react";
-import { Fragment } from "react";
-export default function BoxList(){
-  const [boxes, setBoxes] = useState<string[]>(Array(9).fill("?"));
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
-  const winner = calculateWinner(boxes);
-  function changeBox(targetIndex:number){
-    if(boxes[targetIndex] !== "?" || winner){
-      return;
-    }
-    const nextBoxes = boxes.map((box, boxIndex) =>{
-                if(boxIndex === targetIndex){
-                box = xIsNext ? "X" : "O";
-                }
-              return box;
-              });
-    setBoxes(nextBoxes);
-    setXIsNext(!xIsNext);
+
+type ProductItemProps = {
+  name:string;
+  count:number;
+}
+function ProductItem({name, count}:ProductItemProps){
+  return(<span>{name} {count}個</span>);
+}
+type CountButtonProps = {
+  isAdd:boolean;
+  clickAction:()=>void;
+}
+function CountButton({isAdd, clickAction}:CountButtonProps){
+  return <button onClick={clickAction}>{isAdd ? "+" : "-"}</button>
+}
+type Product = {
+  name:string;
+  count:number;
+}
+const products:Product[] = [
+  {name:"リンゴ", count: 10},
+  {name:"バナナ", count: 8}
+]
+export default function ShoppingCart(){
+  const [productItems, setProductItems] = useState(products.map(product => product));
+  function clickCount(targetName:string, isAdd:boolean){
+    const newProductItem = productItems.map(item =>{
+      if(item.name !== targetName){
+        return item;
+      }
+      if(isAdd){
+        return {...item, count:item.count + 10};
+      }
+      return {...item, count:item.count === 0 ? item.count : item.count - 10};
+    });
+    setProductItems(newProductItem);
   }
   return(
-    <div>
-      {winner && <p>勝者は {winner} !!!!</p>}
-      {boxes.map((box, boxIndex) => {
-        return(
-          <Fragment key={boxIndex}>
-            <button onClick={()=>changeBox(boxIndex)}>{box}</button>
-            {boxIndex % 3 === 2 ? <br/> : ""}
-          </Fragment>
-        );
+    <>
+    {productItems.map(item => {
+    return(
+      <div key={item.name}>
+        <CountButton isAdd={true} clickAction={()=>clickCount(item.name, true)}/>
+        <ProductItem  name={item.name} count={item.count}/>
+        <CountButton isAdd={false} clickAction={()=>clickCount(item.name, false)}/>
+      </div>
+    );
     })}
+    <div>
+          <p>合計: {productItems.reduce((sum, item)=>sum + item.count, 0)}</p>
     </div>
-  );
-}
-
-function calculateWinner(boxes:string[]){
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-  for(const line of lines){
-    const [a, b, c] = line;
-    if(boxes[a] !== "?" && boxes[a] === boxes[b] && boxes[a] === boxes[c]){
-      return boxes[a];
-    }
-  }
-  return null
+    </>
+  )
 }
