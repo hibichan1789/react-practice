4dd5207b54eac90b0db8e25a203997afa29a517c
hibiki <takechi.hibiki1234@gmail.com>
2026-02-22 16:24:11 +0900

    shopping

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,69 @@
-import { useState } from "react";
-
 // src/App.tsx
-type Assets = {
-  cash:number;
-  stock:number;
-  investmentTrust:number;
-}
-type DisplayAssetProps = Assets & {totalAssets:number}
 
-function DisplayMoney({cash, stock, investmentTrust, totalAssets}:DisplayAssetProps){
-  return(
-    <>
-      <ul>
-        <li>CASH: {formatMoney(cash)}</li>
-        <li>STOCK: {formatMoney(stock)}</li>
-        <li>INVESTMENT TRUST: {formatMoney(investmentTrust)}</li>
-      </ul>
-      <p>TOTAL MONEY: {formatMoney(totalAssets)}</p>
-    </>
-  );
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+type Product = {
+  name:string;
+  price:number;
 }
-function BuyButton({transactionAmount, cash, isStock, handleClick}:{transactionAmount:number, cash:number, isStock:boolean, handleClick:()=>void}){
-  return <button disabled={transactionAmount > cash} onClick={handleClick}>買う ({isStock? "株式" : "投資信託"} +{formatMoney(transactionAmount)}, 現金 -{formatMoney(transactionAmount)})</button>
+type TotalPriceProps = {
+  price:number;
+  priceTaxIncluded:number;
 }
-function SalaryButton({salaryAmount, handleClick}:{salaryAmount:number, handleClick:()=>void}){
-  return <button onClick={handleClick}>給料日(現金+ {formatMoney(salaryAmount)})</button>
+function DisplayTotalPrice({price, priceTaxIncluded}:TotalPriceProps){
+  return <p>小計: {formatPrice(price)} (税込み:{formatPrice(priceTaxIncluded)})</p>
 }
-function DisplayLogs({logs}:{logs:string[]}){
+function ProductByButton({name, price, handleClick}:Product&{handleClick:()=>void}){
+  return <button onClick={handleClick}>{name} {price}</button>
+}
+function DisplayLogs({logs, handleDelete}:{logs:ProductLog[], handleDelete:(id:string)=>void}){
   return(
     <ul>
-      {logs.map((log, logIndex) => <li key={logIndex}>{log}</li>)}
+      {logs.map(log=><li key={log.id}>{log.name} {formatPrice(log.price)}<button onClick={()=>handleDelete(log.id)}>削除</button></li>)}
     </ul>
   );
 }
-
-const initialAssets:Assets = {
-  cash:100000,
-  stock:700000,
-  investmentTrust:200000,
-}
-const transactionAmount = 5*10**3;
-const salaryAmount = 5*10**3;
-export default function AssetsApp(){
-  const [currentAssets, setCurrentAssets] = useState<Assets>(initialAssets);
-  const [logs, setLogs] = useState<string[]>([]);
-  const totalAssets:number = Object.values(currentAssets).reduce((total, money)=>total+money, 0);
-  function handleBuyClick(isStock:boolean){
-    if(isStock){
-      setCurrentAssets({...currentAssets, cash:currentAssets.cash - transactionAmount, stock:currentAssets.stock + transactionAmount})
-      const newLog = `株式を${formatMoney(transactionAmount)}買いました (${formatMoney(totalAssets)})`;
-      const newLogs = [...logs, newLog];
-      setLogs(newLogs);
-    }
-    else{
-      setCurrentAssets({...currentAssets, cash:currentAssets.cash - transactionAmount, investmentTrust:currentAssets.investmentTrust + transactionAmount})
-      const newLog = `投資信託を${formatMoney(transactionAmount)}買いました (${formatMoney(totalAssets)})`;
-      const newLogs = [...logs, newLog];
-      setLogs(newLogs);
-    }
+const products:Product[] = [
+  {name:"リンゴ", price:150},
+  {name:"バナナ", price:100},
+  {name:"ミカン", price:80}
+]
+type ProductLog = Product&{id:string};
+const TAX = 1.1;
+export default function ShoppingApp(){
+  const [productLogs, setProductLogs] = useState<ProductLog[]>([]);
+  const currentTotal = productLogs.reduce((total, log)=> total + log.price, 0);
+  function handleBuy(name:string, price:number){
+    const newLogs:ProductLog[] = [...productLogs, {id:uuidv4(), name, price}]
+    setProductLogs(newLogs);
   }
-  function handleSalaryClick(){
-    setCurrentAssets({...currentAssets, cash:currentAssets.cash + salaryAmount});
-    const newLog = `給料から${formatMoney(transactionAmount)}証券口座に入れました (${formatMoney(totalAssets)})`;
-    const newLogs = [...logs, newLog];
-    setLogs(newLogs);
+  function handleReset(){
+    setProductLogs([]);
   }
+  function handleDelete(id:string){
+    const deletedLogs = productLogs.filter(log => log.id !== id);
+    setProductLogs(deletedLogs);
+  }
+  const priceTaxIncluded = calculateTaxIncluded(currentTotal);
   return(
     <>
-      <DisplayMoney cash={currentAssets.cash} stock={currentAssets.stock} investmentTrust={currentAssets.investmentTrust} totalAssets={totalAssets}/>
-      <BuyButton transactionAmount={transactionAmount} cash={currentAssets.cash} isStock={true} handleClick={()=>handleBuyClick(true)}/>
-      <BuyButton transactionAmount={transactionAmount} cash={currentAssets.cash} isStock={false} handleClick={()=>handleBuyClick(false)}/>
-      <SalaryButton salaryAmount={salaryAmount} handleClick={()=>handleSalaryClick()}/>
-      {checkCurrentCash(currentAssets.cash, totalAssets) ? <p style={{color:"green"}}>OK:キャッシュを維持できています</p> : <p style={{color:"red"}}>NG:現金比率が危険です</p>}
-      <DisplayLogs logs={logs}/>
+      <DisplayTotalPrice price={currentTotal} priceTaxIncluded={priceTaxIncluded}/>
+      <div>
+        {products.map((product, productIndex)=><ProductByButton key={productIndex} name={product.name} price={product.price} handleClick={()=>handleBuy(product.name, product.price)} />)}
+        <button onClick={handleReset}>リセット</button>
+      </div>
+      <DisplayLogs logs={productLogs} handleDelete={handleDelete}/>
     </>
   );
 }
 
 // utils関数
-function formatMoney(money:number){
-  return money.toLocaleString("ja-JP", {
-    style:"currency",
-    currency: "JPY"
-  })
+function calculateTaxIncluded(price:number):number{
+  return Math.floor(price * TAX);
 }
-function checkCurrentCash(cash:number, totalAssets:number):boolean{
-  const isSafe = cash/totalAssets >= 0.10;
-  return isSafe;
+function formatPrice(price:number):string{
+  return price.toLocaleString("ja-JP", {
+    style:"currency",
+    currency:"JPY"
+  })
 }
